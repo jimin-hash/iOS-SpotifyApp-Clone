d2701422b259c31305f3757ed7b1bbac50e48b3f
jimin-hash <jimin.park2125@gmail.com>
2022-02-20 18:41:59 +0900

    Feat: Spotify Auth Set UP

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    private let signInbutton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "Spotify"
-        view.backgroundColor = .green
+        view.backgroundColor = .systemGreen
+        view.addSubview(signInbutton)
+        signInbutton.addTarget(self, action: #selector(didTabpSignIn), for: .touchUpInside)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        signInbutton.frame = CGRect(x: 20, y: view.height - 50 - view.safeAreaInsets.bottom, width: view.width - 40, height: 50)
+    }
+    
+    @objc func didTabpSignIn() {
+        let vc = AuthViewController()
+        vc.completionHandler = { [weak self] success in
+            DispatchQueue.main.async {
+                self?.handlerSignIn(success: success)
+            }
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handlerSignIn(success: Bool) {
+        // Log user in or well ar them for error
+        
+    }
 }
--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -11,7 +11,22 @@ import Foundation
 final class AuthManager {
     static let shard = AuthManager()
     
+    struct Constants {
+        static let clientID = "3708d1d1583545f3b0ebcb7175942424"
+        static let clientSecret = "d3cf043f4014475fbbe944ffcf769d5e"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURL = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURL)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
+    
     
     var isSinged: Bool {
         return false
@@ -27,5 +42,18 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)
+    ) {
+        // get Token
+    }
+    
+    private func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
